b48438ddec6989cf7976a5a70942b7ee18cf9090
Шаповалов Олександр web2025-3 <alexandrshapovalov11@gmail.com>
2026-05-22 08:27:17 +0300

    Fix flake8 formatting

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-
 from app.database import db
 
 
@@ -7,7 +6,6 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-
 
     def to_dict(self):
         return {
