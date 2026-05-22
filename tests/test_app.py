a795236f9d4df4dcbc303ab3939e3dbcc8ccb0f4
Шаповалов Олександр web2025-3 <alexandrshapovalov11@gmail.com>
2026-05-22 08:35:01 +0300

    Fix flake8

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,10 @@
 import os
 import sys
+import pytest
+
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import pytest
 from app.app import create_app
 
 
@@ -16,7 +17,6 @@ os.environ["POSTGRES_DB"] = "devops_lab"
 @pytest.fixture
 def client():
     app = create_app()
-
     app.config["TESTING"] = True
 
     with app.test_client() as client:
@@ -25,14 +25,9 @@ def client():
 
 def test_home(client):
     response = client.get("/")
-
     assert response.status_code == 200
 
 
 def test_create_task(client):
-    response = client.post(
-        "/tasks",
-        json={"title": "Test task"}
-    )
-
+    response = client.post("/tasks", json={"title": "Test task"})
     assert response.status_code == 201
